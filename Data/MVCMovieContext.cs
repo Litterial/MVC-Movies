cc574b69db5472fb2fde251ec75cb18573b2f0d5
Litterial <yungice12@gmail.com>
2019-12-18 19:28:59 -0600

    made a function to create datbase

--- Data/MVCMovieContext.cs
+++ Data/MVCMovieContext.cs
@@ -3,7 +3,7 @@ using MVcMovie.Models;
 
 namespace MVcMovie.Data{
     public class MVcMovieContext : DbContext{
-        public MVcMovieContext (DbContextOptions<MVcMovieContext> options) : base(options){}
+        public MVcMovieContext (DbContextOptions<MVcMovieContext> options) : base(options){ Database.EnsureCreated();}
 
         public DbSet<Movie> Movies {get;set;}
     }
